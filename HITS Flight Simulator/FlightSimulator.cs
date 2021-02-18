888e99b96e5bed28edee60e453660ae0dd8f162d
VladimirPrisyach <redbull-8@bk.ru>
2021-02-18 17:22:31 +0700

    Done

---
+++ HITS Flight Simulator/FlightSimulator.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HITS_Flight_Simulator
+{
+    public class FlightSimulator
+    {
+        double dt;
+        const double g = 9.8;
+        const double C = 0.15;
+        const double rho = 1.29;
+
+        public double a;
+        public double v0;
+        public double y0;
+        public double S;
+        public double m;
+        public double k;
+
+        double t;
+        double x;
+        public double y;
+        double vx;
+        double vy;
+        public double time_sum =0;
+
+      
+        public (double, double) TickOfFlight()
+        {
+            time_sum += dt;
+
+            t += dt;
+            vx = vx - k * vx * Math.Sqrt(vx * vx + vy * vy) * dt;
+            vy = vy - (g + k * vy * Math.Sqrt(vx * vx + vy * vy)) * dt;
+
+            x = x + vx * dt;
+            y = y + vy * dt;
+
+            return (x, y);
+        }
+
+        public (double,double) MaxHL()
+        {
+
+            return(v0 * v0 * Math.Sin(a) * Math.Sin(a) / (2 * g) + 2 + y0, v0 * v0 * Math.Sin(2 * a) / g + 2);
+        }
+
+        public void StartOfFlight(double dtime )
+        {
+            k = 0.5 * C * S * rho / m;
+            dt = dtime;
+            vx = v0 * Math.Cos(a);
+            vy = v0 * Math.Sin(a);
+            t = 0;
+            x = 0;
+            y = y0;
+        }
+    }
+}
--- HITS Flight Simulator/Form1.cs
+++ HITS Flight Simulator/Form1.cs
@@ -12,70 +12,34 @@ namespace HITS_Flight_Simulator
 {
     public partial class Form1 : Form
     {
+        FlightSimulator fg = new FlightSimulator();
         public Form1()
         {
             InitializeComponent();
         }
 
-        double dt;
-        const double g = 9.8;
-        const double C = 0.15;
-        const double rho = 1.29;
-
-        double a;
-        double v0;
-        double y0;
-        double S;
-        double m;
-        double k;
-
-        double t;
-        double x;
-        double y;
-        double vx;
-        double vy;
-        double time_sum;
+        
         private void button1_Click(object sender, EventArgs e)
         {
-            time_sum = 0;
-            time_timer.Text = $"Времени прошло {time_sum}";
-            a = (double)edAngle.Value * Math.PI / 180;
-            v0 = (double)edSpeed.Value;
-            y0 = (double)edHeight.Value;
-            m = (double)edWeight.Value;
-            S = (double)edSquare.Value;
-            k = 0.5 * C * S * rho / m;
-            dt = (double)timer1.Interval / 1000;
-
-            vx = v0 * Math.Cos(a);
-            vy = v0 * Math.Sin(a);
-
-
-            t = 0;
-            x = 0;
-            y = y0;
+            
+            time_timer.Text = $"Времени прошло {fg.time_sum}";
+            InitFlightComp();
+            fg.StartOfFlight((double)timer1.Interval / 1000);
             chart1.Series[0].Points.Clear();
-            chart1.Series[0].Points.AddXY(x, y);
+            chart1.Series[0].Points.AddXY(0, fg.y0);
             timer1.Start();
-            double max_height = v0 * v0 * Math.Sin(a) * Math.Sin(a) / (2 * g) + 2;
-            double max_lenght = v0 * v0 * Math.Sin(2 * a) / g + 2;
+            double max_height = fg.MaxHL().Item1;
+            double max_lenght = fg.MaxHL().Item2;
             chart1.ChartAreas[0].AxisY.Maximum = max_height;
             chart1.ChartAreas[0].AxisX.Maximum = max_lenght;
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            time_sum += dt;
-            time_timer.Text = $"Времени прошло {time_sum} мс";
-            t += dt;
-            vx = vx - k * vx * Math.Sqrt(vx * vx + vy * vy) * dt;
-            vy = vy - (g + k * vy * Math.Sqrt(vx * vx + vy * vy)) * dt;
-            
-            x = x + vx * dt;
-            y = y + vy * dt;
-
-            chart1.Series[0].Points.AddXY(x, y);
-            if (y <= 0) timer1.Stop();
+            (double,double) XY = fg.TickOfFlight();
+            time_timer.Text = $"Времени прошло {fg.time_sum}";
+            chart1.Series[0].Points.AddXY(XY.Item1, XY.Item2);
+            if (XY.Item2 <= 0) timer1.Stop();
 
         }
 
@@ -86,11 +50,19 @@ namespace HITS_Flight_Simulator
                 timer1.Stop();
                 button2.Text = "Continue";
             }
-            else if (!timer1.Enabled && y > 0)
+            else if (!timer1.Enabled && fg.y > 0)
             {
                 timer1.Start();
                 button2.Text = "Stop";
             }
+        }
+        private void InitFlightComp()
+        {
+            fg.a = (double)edAngle.Value * Math.PI / 180;
+            fg.v0 = (double)edSpeed.Value;
+            fg.y0 = (double)edHeight.Value;
+            fg.m = (double)edWeight.Value;
+            fg.S = (double)edSquare.Value;
         }
     }
 }
